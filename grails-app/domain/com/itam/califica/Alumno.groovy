b8d0eab1931f05ef7603f201f48b08c2fc0253e2
Gerardo Meza <mezagg@gmail.com>
2016-06-21 20:04:51 -0500

    se agregan atributos de alumno

--- grails-app/domain/com/itam/califica/Alumno.groovy
+++ grails-app/domain/com/itam/califica/Alumno.groovy
@@ -1,7 +1,13 @@
 package com.itam.califica
 
 class Alumno {
-
+    
+    Integer id
+    String nombre
+    String apellidoPaterno
+    String apellidoMaterno
+    String matricula
+    
     static constraints = {
     }
 }
